2e2a0562f2e510318a83e9e23b311522d89246a9
EthanRoy4 <sunilselvaraj30@gmail.com>
2024-04-04 11:39:53 +0530

    Created new local respository

---
+++ src/main/java/com/git/project/git/project/App.java
@@ -0,0 +1,13 @@
+package com.git.project.git.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/git/project/git/project/MyGitProject1.java
@@ -0,0 +1,15 @@
+package com.git.project.git.project;
+
+public class MyGitProject1 {
+
+	public static void main(String[] args) {
+		System.out.println("print my project");
+	}
+    
+	public void print() {
+		String name;
+		System.out.println("This is my project");
+		
+	}
+	
+}
